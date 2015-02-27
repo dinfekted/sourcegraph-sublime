e0d63d2e76c6960c86d09a4f2299a1f129427a52
Leonid Shagabutdinov <leonid@shagabutidnov.com>
2015-02-27 16:53:56 +0600

    error and status reporting was complete

--- commands.py
+++ commands.py
@@ -4,6 +4,8 @@ import sublime_plugin
 import os
 from . import utilities
 
+import webbrowser
+
 # based on Stackoverflow Search Plugin by Eric Martel (emartel@gmail.com / www.ericmartel.com)
 # @author Leonid Shagabutdinov <leonid@shagabutdinov.com>
 
@@ -21,19 +23,19 @@ class SourcegraphSearchSelectionCommand(sublime_plugin.TextCommand):
 
 class SourcegraphSearchFromInputCommand(sublime_plugin.WindowCommand):
 
-    def run(self):
-        # Get the search item
-        self.window.show_input_panel('Search Sourcegraph for', '',
-            self.on_done, self.on_change, self.on_cancel)
+  def run(self):
+    # Get the search item
+    self.window.show_input_panel('Search Sourcegraph for', '',
+      self.on_done, self.on_change, self.on_cancel)
 
-    def on_done(self, input):
-        utilities.gotoSourcegraph('/search', {'q': input})
+  def on_done(self, input):
+    utilities.gotoSourcegraph('/search', {'q': input})
 
-    def on_change(self, input):
-        pass
+  def on_change(self, input):
+    pass
 
-    def on_cancel(self):
-        pass
+  def on_cancel(self):
+    pass
 
 
 class SourcegraphJumpToDefinition(sublime_plugin.TextCommand):
@@ -42,23 +44,34 @@ class SourcegraphJumpToDefinition(sublime_plugin.TextCommand):
     sublime.set_timeout_async(self.execute, 0)
 
   def execute(self):
-    response = utilities.call_srclib(self.view, self.view.sel()[0])['Def']
-    if 'Repo' in response:
-      # TODO: Resolve to local file - waiting for Src API to expose method for this
-      url = utilities.BASE_URL + "/%s/.%s/%s/.def/%s" % (response['Repo'],
-          response['UnitType'], response['Unit'], response['Path'])
-      webbrowser.open_new_tab(url)
+    response = utilities.call_srclib(self.view, self.view.sel()[0],
+      ['--no-examples'])
+
+    if 'Def' not in response:
+      utilities.StatusTimeout(self.view, 'definition not found')
       return
 
-    file_path = response['File']
+    definition = response['Def']
+
+    if 'Repo' in definition:
+      # TODO: Resolve to local file - waiting for Src API to expose method for this
+      url = utilities.BASE_URL + "/%s/.%s/%s/.def/%s" % (definition['Repo'],
+          definition['UnitType'], definition['Unit'], definition['Path'])
+      webbrowser.open_new_tab(url)
+      utilities.StatusTimeout(self.view, 'definition is opened in browser')
+      return
+
+    file_path = definition['File']
     if not os.path.isfile(file_path):
-      log.error('File %s does not exist.' % file_path)
+      utilities.StatusTimeout(self.view, 'definition found but file %s does ' +
+        'not exist.' % file_path)
       return
 
     view = self.view.window().open_file(file_path)
-    sublime.set_timeout(lambda: show_location(view, response['DefStart'],
-      response['DefEnd']), 10)
+    sublime.set_timeout(lambda: utilities.show_location(view,
+      definition['DefStart'], definition['DefEnd']), 10)
 
+    utilities.StatusTimeout(self.view, 'definition found')
 
 class SourcegraphDescribeCommand(sublime_plugin.TextCommand):
 
@@ -66,33 +79,41 @@ class SourcegraphDescribeCommand(sublime_plugin.TextCommand):
     sublime.set_timeout_async(self.execute, 0)
 
   def execute(self):
-    response = utilities.call_srclib(self.view, self.view.sel()[0])['Def']
-    choices = [response['Name']]
+    response = utilities.call_srclib(self.view, self.view.sel()[0],
+      ['--no-examples'])
 
-    if 'DocHTML' in response:
-        choices.append(utilities.strip_tags(response['DocHTML']))
+    if 'Def' not in response:
+      utilities.StatusTimeout(self.view, 'definition not found')
+      return
 
-    for k, v in response['Data'].items():
-        choices.append('%s: %s' % (k, str(v)))
+    definition = response['Def']
+
+    choices = [definition['Name']]
+
+    if 'DocHTML' in definition:
+      choices.append(utilities.strip_tags(definition['DocHTML']))
+
+    for k, v in definition['Data'].items():
+      choices.append('%s: %s' % (k, str(v)))
 
     # ST2 lacks view.show_popup_menu
     if hasattr(self.view, "show_popup_menu"):
-        self.view.show_popup_menu(choices, self.goto_describe_link,
+      self.view.show_popup_menu(choices, self.goto_describe_link,
             sublime.MONOSPACE_FONT)
     else:
-        # TODO(sqs): multi-line rows don't work in show_quick_panel
-        self.view.window().show_quick_panel(choices, self.goto_describe_link,
-            sublime.MONOSPACE_FONT)
+      # TODO(sqs): multi-line rows don't work in show_quick_panel
+      self.view.window().show_quick_panel(choices, self.goto_describe_link,
+        sublime.MONOSPACE_FONT)
 
   def goto_describe_link(self, picked):
-      if picked == -1 or len(self.results) == 0:
-          return
+    if picked == -1 or len(self.results) == 0:
+      return
 
-      sym = self.results[picked]
-      url = utilities.BASE_URL + ('/%s/symbols/%s/%s' % (quote(sym['repo']),
-          quote(sym['lang']), quote(sym['path'])))
+    sym = self.results[picked]
+    url = utilities.BASE_URL + ('/%s/symbols/%s/%s' % (quote(sym['repo']),
+      quote(sym['lang']), quote(sym['path'])))
 
-      webbrowser.open_new_tab(url)
+    webbrowser.open_new_tab(url)
 
 
 class SourcegraphUsagesCommand(sublime_plugin.TextCommand):
--- utilities.py
+++ utilities.py
@@ -6,9 +6,9 @@ import threading
 import webbrowser
 
 try:
-    from urllib.parse import urlencode
+  from urllib.parse import urlencode
 except ImportError:
-    from urllib import urlencode
+  from urllib import urlencode
 
 import json
 import subprocess
@@ -17,45 +17,49 @@ import os
 BASE_URL = "https://sourcegraph.com"
 VIA = "sourcegraph-sublime-1"
 
-# log = logging.getLogger("sourcegraph")
-# stderr_hdlr = logging.StreamHandler(sys.stderr)
-# stderr_hdlr.setFormatter(logging.Formatter("%(name)s: %(levelname)s: %(message)s"))
-# log.handlers = [stderr_hdlr]
-# log.setLevel(logging.INFO)
-
 settings = {}
 
 def getenv():
-    env = os.environ
-    gopath = settings.get('gopath')
-    if gopath:
-        env['GOPATH'] = gopath
-    goroot = settings.get('goroot')
-    if goroot:
-        env['GOROOT'] = goroot
-    return env
+  env = os.environ
+  gopath = settings.get('gopath')
+  if gopath:
+      env['GOPATH'] = gopath
+  goroot = settings.get('goroot')
+  if goroot:
+      env['GOROOT'] = goroot
+  return env
 
 def plugin_loaded():
-    global settings
-    settings = sublime.load_settings('Sourcegraph.sublime-settings')
+  global settings
+  settings = sublime.load_settings('Sourcegraph.sublime-settings')
 
 def gotoSourcegraph(path, params):
-    params['_via'] = VIA
-    webbrowser.open_new_tab(BASE_URL + path + '?' + urlencode(params))
+  params['_via'] = VIA
+  webbrowser.open_new_tab(BASE_URL + path + '?' + urlencode(params))
 
 def textFromViewSel(view, sel):
-    # if the user didn't select anything, search the currently highlighted word
-    if sel.empty():
-        sel = view.word(sel)
-    return view.substr(sel)
+  if sel.empty():
+    sel = view.word(sel)
+
+  return view.substr(sel)
 
 def show_location(view, start, end, retries=0):
   if not view.is_loading():
-    def_region = sublime.Region(start, end)
-
     view.sel().clear()
-    view.sel().add(def_region)
+    view.sel().add(sublime.Region(start, end))
     view.show_at_center(start)
+
+    view.run_command('move', {
+      'by': 'characters',
+      'forward': False,
+      'extend': True
+    })
+
+    view.run_command('move', {
+      'by': 'characters',
+      'forward': True,
+      'extend': True
+    })
   else:
     if retries < 10:
       sublime.set_timeout(lambda: show_location(view, start, end, retries+1), 10)
@@ -64,8 +68,11 @@ def show_location(view, start, end, retries=0):
       print('timed out waiting for file load')
 
 def call_srclib(view, sel, tail = []):
+  view.set_status('sourcegraph_command', 'sourcegraph: executing src command...')
+  StatusAnimation(view).animate()
+
   # import time
-  # time.sleep(2) # for developing
+  # time.sleep(5) # for developing
   command = [
     settings.get('which_src'),
     "api",
@@ -78,53 +85,94 @@ def call_srclib(view, sel, tail = []):
 
   command += tail
 
-  data = check_output(command, env=getenv())
-  return json.loads(data.decode("utf8"))
+  try:
+    data = check_output(command, env=getenv())
+    return json.loads(data.decode("utf8"))
+  except Exception as error:
+    output = view.window().new_file()
+    output.set_scratch(True)
+    message = (sublime.load_resource('Packages/Sourcegraph/error.txt') +
+      ' ' + str(error))
+
+    output.run_command('insert', {'characters': message})
+    utilities.StatusTimeout(view, 'error has occured')
+  finally:
+    view.erase_status('sourcegraph_command')
+
+class StatusTimeout():
+
+  def __init__(self, view, status, timeout = 5000):
+    self.view = view
+    self.view.set_status('sourcegraph_status', 'sourcegraph: ' + status)
+    sublime.set_timeout(self._hide, timeout)
+
+  def _hide(self):
+    self.view.erase_status('sourcegraph_status')
+
+class StatusAnimation():
+
+  def __init__(self, view):
+    self.view = view
+
+  def animate(self):
+    sublime.set_timeout(self._animate, 250)
+
+  def _animate(self):
+    status = self.view.get_status('sourcegraph_command')
+    if status != None and status.endswith('...'):
+      if status.endswith('.' *  10):
+        status = status[:-7]
+
+      self.view.set_status('sourcegraph_command', status + '.')
+      sublime.set_timeout(self._animate, 250)
 
 # Python 2.6 doesn't have subprocess.check_output, so backport it. (From
 # https://gist.github.com/edufelipe/1027906.)
 def check_output(*popenargs, **kwargs):
-    r"""Run command with arguments and return its output as a byte string.
+  r"""Run command with arguments and return its output as a byte string.
 
-    Backported from Python 2.7 as it's implemented as pure python on stdlib.
+  Backported from Python 2.7 as it's implemented as pure python on stdlib.
 
-    >>> check_output(['/usr/bin/python', '--version'])
-    Python 2.6.2
-    """
-    process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs)
-    output, unused_err = process.communicate()
-    retcode = process.poll()
-    if retcode:
-        cmd = kwargs.get("args")
-        if cmd is None:
-            cmd = popenargs[0]
-        error = subprocess.CalledProcessError(retcode, cmd)
-        error.output = output
-        raise error
-    return output
+  >>> check_output(['/usr/bin/python', '--version'])
+  Python 2.6.2
+  """
+  process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs)
+  output, unused_err = process.communicate()
+  retcode = process.poll()
+
+  if retcode:
+    cmd = kwargs.get("args")
+    if cmd is None:
+      cmd = popenargs[0]
+
+    error = subprocess.CalledProcessError(retcode, cmd)
+    error.output = output
+    raise error
+
+  return output
 
 # from http://stackoverflow.com/a/925630
 try:
-    # python 3
-    from html.parser import HTMLParser
+  # python 3
+  from html.parser import HTMLParser
 except:
-    # python 2
-    from HTMLParser import HTMLParser
+  # python 2
+  from HTMLParser import HTMLParser
 
 class MLStripper(HTMLParser):
 
-    def __init__(self):
-        super().__init__()
-        self.reset()
-        self.fed = []
+  def __init__(self):
+    super().__init__()
+    self.reset()
+    self.fed = []
 
-    def handle_data(self, d):
-        self.fed.append(d)
+  def handle_data(self, d):
+    self.fed.append(d)
 
-    def get_data(self):
-        return ''.join(self.fed)
+  def get_data(self):
+    return ''.join(self.fed)
 
 def strip_tags(html):
-    s = MLStripper()
-    s.feed(html)
-    return s.get_data()
+  s = MLStripper()
+  s.feed(html)
+  return s.get_data()
